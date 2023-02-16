2124521b05be209ead325ac8b1c99f9672c92b65
Sourcery AI <>
2023-02-16 10:57:28 +0000

    'Refactored by Sourcery'

--- main.py
+++ main.py
@@ -8,9 +8,7 @@ SITE_NAME = 'https://youtube.com/'
 @app.route('/<path:path>')
 def proxy(path):
   resp = get(f'{SITE_NAME}{path}')
-  if resp.headers.get('content-type'):
-    return resp.content
-  return resp.text
+  return resp.content if resp.headers.get('content-type') else resp.text
   
 if __name__ == '__main__':
   app.run(host='0.0.0.0', port=8080)
